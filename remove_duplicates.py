f4826de673e21d729b287c04e4365c30e1312b6e
SURESH KANURI <137312698+SureshThedatanerd@users.noreply.github.com>
2023-07-27 21:53:29 +0530

    remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,14 +1,6 @@
-'''a = [1,2,3,1,4,5,2]
-for i in range(0,len(a)):
-    for j in range(i+1,len(a)):
-        if a[i]==a[j]:
-            a.pop(j)
-    print(a[i])'''
-
-a = [1,2,3,1,4,5,2]
+a = [1,2,3,1,4,5,2,3,5,8]
 seen = []
 for i in a:
     if i not in seen:
         seen.append(i)
-    for j in seen:
-        print(seen[j])
+print(seen)
